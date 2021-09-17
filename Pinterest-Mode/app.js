f8d54879d5612e764f505a219aa746c99b764a6d
Daniel Rodriguez <dev.daniel_rodriguez_gomez@yahoo.com>
2021-09-16 23:03:14 -0500

    added Images Gallery Interface

--- Pinterest-Mode/app.js
+++ Pinterest-Mode/app.js
@@ -3,12 +3,26 @@ const log = console.log;
 const er = console.error;
 const URL_API = 'https://dog.ceo/api/breed/hound/images';
 const main = id("main");
+const tooggleTheme = id("tooggle-theme");
+const tooggleIcon = id("tooggle-icon");
+const tooggleText = id("tooggle-text");
 
 fetch(URL_API)
     .then(answer => answer.json())
     .then(data => {
-        for (let index = 0; index < 50; index++) {
-            main.innerHTML += `<img src="${data.message[index]}">`;
+        for (let index = 0; index < 150; index++) {
+            main.innerHTML += `<img class="images" src="${data.message[index]}">`;
         }
     })
 .catch(error => log(error))
+
+tooggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (tooggleIcon.src.includes("dark.svg")) {
+        tooggleIcon.src = "img/light.svg";
+        tooggleText.textContent = "Activar Light";
+    } else {
+        tooggleIcon.src = "img/dark.svg";
+        tooggleText.textContent = "Activar Dark";
+    }
+})
