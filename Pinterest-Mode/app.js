9f15a06f7a0dad99f7cb5717c759fb6bea13d9ed
Daniel Rodriguez <dev.daniel_rodriguez_gomez@yahoo.com>
2021-09-25 22:06:01 -0500

    Images EndPoint change

--- Pinterest-Mode/app.js
+++ Pinterest-Mode/app.js
@@ -1,20 +1,35 @@
 const id = document.getElementById.bind(document);
 const log = console.log;
 const er = console.error;
-const URL_API = 'https://dog.ceo/api/breed/hound/images';
+const URL_API = 'https://dog.ceo/api/breed/hound/images/random/150';
 const main = id("main");
 const tooggleTheme = id("tooggle-theme");
 const tooggleIcon = id("tooggle-icon");
 const tooggleText = id("tooggle-text");
 
-fetch(URL_API)
-    .then(answer => answer.json())
-    .then(data => {
-        for (let index = 0; index < 150; index++) {
-            main.innerHTML += `<img class="images" src="${data.message[index]}">`;
-        }
-    })
-.catch(error => log(error))
+// fetch(URL_API)
+//     .then(answer => answer.json())
+//     .then(data => {
+//         data.message.forEach(element => {
+//             main.innerHTML += `<img class="images" src="${element}">`;
+//         });
+//     })
+// .catch(error => log(error))
+
+
+const fetchData = async () => {
+    try {
+        const ans = await fetch(URL_API);
+        const data = await ans.json();
+        data.message.forEach(element => {
+            main.innerHTML += `<img class="images" src="${element}">`;
+        });
+    } catch (error) {
+        er(error);
+    }
+}
+
+fetchData()
 
 tooggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
